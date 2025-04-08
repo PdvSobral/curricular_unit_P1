113173498600da814e106ced68ef06c382586910
Da59Gigas <da59gigas@gmail.com>
2025-04-08 17:51:29 +0000

    Just a little refactoring. Updated "comiler.py" to match some of the changes in the file and properties. Will later upload said file to my GitHub.

--- PROJETO_PRÁTICO/main.c
+++ PROJETO_PRÁTICO/main.c
@@ -20,6 +20,8 @@ The pdf is present in the same repository as this program.
 	#pragma GCC warning "Loaded standard modules."
 #else
 	#include <good_practices.c>
+	// Basicly imports the four modules above
+	// Makes so that int, unsigned and other lables/macros are not usable
 	#pragma GCC warning "Loaded custom module."
 #endif
 
