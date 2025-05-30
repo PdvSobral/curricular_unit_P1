a6cbfe4260d1c38a74d34049cc91b7d88c7f43ed
alexandredomingos <alexandredomingos@ipvc.pt>
2025-05-30 15:20:27 +0100

    alteração de um print de password, porque o Alex é burro e faz debugging para cacete

--- PROJETO_PRÁTICO/main.c
+++ PROJETO_PRÁTICO/main.c
@@ -169,7 +169,6 @@ uint8_t regist() {
         if (strlen2(buffer) > 0 && strlen2(buffer) <= MAX_PASSWORD_LENGTH){
 			printf("Confirm Password: ");
         	get_password(buffer, MAX_PASSWORD_LENGTH);
-			printf("Pass1: %s, Pass2: %s\n", password1, buffer);
 			if(strcmp(password1, buffer) == 0) break;
 			printf("Password is not the same!\n");
 			pause_();
