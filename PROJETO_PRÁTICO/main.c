39b1f44016a17eff14a3a87f45e8ecac2a112bf6
PdvSobral <->
2025-06-01 16:06:30 +0000

    Wrote login2

--- PROJETO_PRÁTICO/main.c
+++ PROJETO_PRÁTICO/main.c
@@ -159,6 +159,150 @@ uint8_t login(uint8_t account_flag_type){
 	return 0;  // sucessfull
 }
 
+uint8_t login2(uint8_t account_flag_type){
+	clear_screen();
+	cabecalho("LOGIN MENU", CABECALHO_LEN);
+	reset_line(CABECALHO_LEN);
+	printf("│ [*] Account ID  :");
+	printf("\033[%uC│\n", CABECALHO_LEN - 20);
+	printf("│ [ ] Password:");
+	printf("\033[%uC│\n", CABECALHO_LEN - 16);
+	for (uint8_t i = 0; i < CABECALHO_LEN; i++) printf("─");
+	printf("\n");
+	reset_line(CABECALHO_LEN);
+	printf("│ [ ] Login   [ ] Cancel");
+	printf("\033[%uC│\n", CABECALHO_LEN - 25);
+	printf("└");
+	for(uint8_t _index = 0; _index<CABECALHO_LEN-2; _index++){
+		printf("─");
+	} printf("┘");
+	printf("\033[4A\033[999D\033[20C");
+	fflush(stdout);
+	uint8_t state=0; //0 - account is  | 1 - password id   | login 'button'
+	uint8_t temp;
+	char ch;
+	char account_id[6]; account_id[0] = 0x00;
+	char password[MAX_PASSWORD_LENGTH + 1]; password[0] = 0x00;
+	char md5_hash[33];
+	while (1){
+		// chars especiais tipo setas
+		if ((ch = getch()) == 27) {
+			if ((ch = getch()) == 91) {
+				if ((ch = getch()) == 65 && state != 0){  // up arrow pressed
+					// printf("↑");
+					switch(state){
+						case 1: printf("\033[999D\033[3C \033[1A\033[5D\033[3C*\033[%dC", 16 + strlen2(account_id)); break;
+						case 2: printf(" \033[1D\033[2A*\033[%dC", 12 + strlen2(password)); break;
+						case 3: printf(" \033[13D*\033[1D"); break;
+					}
+					state--;
+				}
+				else if (ch == 66 && state != 3){		 // down arrow pressed
+					// printf("↓");
+					switch(state){
+						case 0: printf("\033[999D\033[3C \033[1B\033[5D\033[3C*\033[%dC", 12 + strlen2(password)); break;
+						case 1: printf("\033[999D\033[3C \033[2B\033[5D\033[3C*\033[1D"); break;
+						case 2: printf(" \033[11C*\033[1D"); break;
+					}
+					state++;
+				}
+			}
+		}
+		else {
+			if (state == 0 && strlen2(account_id) <= 5){
+				// printf("%d", ch);
+				if ((ch == 0x08 || ch == 0x7F) && strlen2(account_id) != 0){   //  Delete and backspace
+					if (strlen2(account_id) > 0){
+						printf("\033[1D \033[1D");
+						temp = strlen2(account_id);
+						account_id[temp-1] = 0x00;
+					}
+				} else {
+					if (ch >= 0x30 && ch <= 0x39 && strlen2(account_id) < 5){
+						putchar(ch);
+						temp = strlen2(account_id);
+						account_id[temp] = ch;
+						account_id[temp+1] = 0x00;
+					}
+				}
+			}
+			if (state == 1 && strlen2(password) <= MAX_PASSWORD_LENGTH){
+				//printf("%d ", ch);
+				if ((ch == 0x08 || ch == 0x7F) && strlen2(password) != 0){   //  Delete and backspace
+					if (strlen2(password) > 0){
+						printf("\033[1D \033[1D");
+						temp = strlen2(password);
+						password[temp-1] = 0x00;
+					}
+				} else {
+					if ((ch >= 0x20 || (int32_t) ch < 0x00) && strlen2(password) < MAX_PASSWORD_LENGTH){
+						putchar('*');
+						temp = strlen2(password);
+						password[temp] = ch;
+						password[temp+1] = 0x00;
+					}
+				}
+			}
+			if (state == 2){
+				if (ch == 0x0A){
+					if (strlen2(account_id) == 0){
+						printf("\n\n");
+						reset_line(CABECALHO_LEN);
+						print_between_format("Nothing in the ID fiel!", "\033[31m", CABECALHO_LEN, 1);
+						print_bottom(CABECALHO_LEN, 1);
+						printf("\033[4A\033[3C");
+					} else {
+						if (strlen2(account_id) < 5){
+							printf("\n\n");
+							reset_line(CABECALHO_LEN);
+							print_between_format("Invalid account ID!", "\033[31m", CABECALHO_LEN, 1);
+							print_bottom(CABECALHO_LEN, 1);
+							printf("\033[4A\033[3C");
+						} else {
+							if (strlen2(password) == 0){
+								printf("\n\n");
+								reset_line(CABECALHO_LEN);
+								print_between_format("Nothing in the password field!", "\033[31m", CABECALHO_LEN, 1);
+								print_bottom(CABECALHO_LEN, 1);
+								printf("\033[4A\033[3C");
+							} else {
+								ACCOUNT* my_user = get_user_by_id(USER_DATABASE, account_id);
+								hash_md5(password, md5_hash);
+								if (my_user == NULL || my_user->type == account_flag_type || strcmp(my_user->password, md5_hash)!=0){
+									free(my_user);
+									printf("\n\n");
+									reset_line(CABECALHO_LEN);
+									print_between_format("INVALID CREDENTIALS!", "\033[31m", CABECALHO_LEN, 1);
+									print_bottom(CABECALHO_LEN, 1);
+									printf("\033[4A\033[3C");
+								} else {
+									print_middle(CABECALHO_LEN, 1);
+									print_between_format("LOGIN SUCESSFUL!", "\033[32m", CABECALHO_LEN, 1);
+									print_bottom(CABECALHO_LEN, 1);
+									CURRENT_LOGIN = *my_user;
+									free(my_user);
+									pause_();
+									return 0;  // sucessfull
+								}
+							}
+						}
+					}
+				}
+			}
+			if (state == 3){
+				if (ch == 0x0A){
+					printf("\n\n");
+					fflush(stdout);
+					return 1;
+				}
+			}
+		}
+		fflush(stdout);
+	}
+	// Not suposed to get here
+	return 1;
+}
+
 void logout(){
 	CURRENT_LOGIN.uid = 0;
 	CURRENT_LOGIN.name_offset = 999999999;
