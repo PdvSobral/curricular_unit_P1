42f114004bf04208a3377acb36706e637b9aa772
PdvSobral <->
2025-06-04 20:24:35 +0000

    Kind of working.

--- PROJETO_PRÁTICO/testes2.c
+++ PROJETO_PRÁTICO/testes2.c
@@ -39,50 +39,52 @@ void end_file(FILE* file_to_end){
 }
 
 uint8_t read_name_and_append_to_file(const char* file_name, uint8_t max_characters_length){
-	printf("Enter name: ");
-	fflush(stdout);
-	touch(file_name);
-	FILE* file = fopen(file_name, "rb+");
-	if (file == NULL) return 1;
-	fseek(file, 0, SEEK_END);
-	uint8_t ch;
-	uint8_t string_len_in_chars = 0;
-	uint8_t char_len;
-	while (1){
-		if ((ch = getch()) != 0x0A) break;
-		else if (ch == 0x7F || ch == 0x08){  // Backspace && Delete
-			if (string_len_in_chars > 0) {
-				string_len_in_chars--; // len contains the lenght in the screen, so multibytes count as one.
-				do { // but in file we have to take care of each
-					fseek(file, -1, SEEK_CUR);
-					ch = fgetc(file);
-					fseek(file, -1, SEEK_CUR);
-					if (is_utf8_continuation(ch) != 0) break;
-				} while (1);
-				printf("\033[1D \033[1D");
-				fflush(stdout);
-			}
-		}
-		else {
-			char_len = utf8_char_length(ch);
-			if (string_len_in_chars + 1 >= max_characters_length) continue;
-			//name[string_len_in_chars++] = ch;
-			putchar(ch);
-			// Read remaining bytes if multibyte
-			for (uint8_t i = 1; i < char_len; ++i) {
-				ch = getch();
-				//name[string_len_in_chars++] = ch;
-				putchar(ch);
-			}
-			fflush(stdout);
-		}
-	}
-	putchar('\n');
-	fputc(0x0A, file);
-	fseek(file, 3, SEEK_SET);
-	end_file(file);
-	fclose(file);
-	return 0;
+    printf("Enter name: ");
+    fflush(stdout);
+
+    touch(file_name);
+    FILE* file = fopen(file_name, "ab+");
+    if (file == NULL) return 1;
+
+    fseek(file, 0, SEEK_END);
+
+    uint8_t ch;
+    uint8_t string_len_in_chars = 0;
+    uint8_t char_len;
+    while (1){
+        ch = getch();
+        if (ch == 0x0A) break;
+        else if (ch == 0x7F || ch == 0x08){  // Backspace && Delete
+            if (string_len_in_chars > 0) {
+                string_len_in_chars--;
+                do {
+                    fseek(file, -1, SEEK_CUR);
+                    ch = fgetc(file);
+                    fseek(file, -1, SEEK_CUR);
+                    if (is_utf8_continuation(ch) != 0) break;
+                } while (1);
+                printf("\033[1D \033[1D");
+                fflush(stdout);
+            }
+        }
+        else {
+            char_len = utf8_char_length(ch);
+            if (string_len_in_chars + 1 >= max_characters_length) continue;
+            fputc(ch, file); putchar(ch);
+            // Write remaining bytes if multibyte
+            for (uint8_t i = 1; i < char_len; ++i) {
+                ch = getch();
+                fputc(ch, file); putchar(ch);
+            }
+            string_len_in_chars++;
+            fflush(stdout);
+        }
+    }
+    putchar('\n');
+    fputc(0x0A, file);
+    end_file(file);
+    fclose(file);
+    return 0;
 }
 
 int32_t main(){
