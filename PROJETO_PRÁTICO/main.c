a98b90592e399700a5faa4d5f13f87baa8429ed5
alexandredomingos <alexandredomingos@ipvc.pt>
2025-06-11 13:09:05 +0100

    Função 'remove_book' para remoção do livro do diretório e das listas

--- PROJETO_PRÁTICO/main.c
+++ PROJETO_PRÁTICO/main.c
@@ -515,6 +515,39 @@ uint8_t add_book(){
 	return 0;
 }
 
+uint8_t remove_book(){
+	//FIXME: Otimizar se houver tempo. Pedro: Em vex de abrir livro, apenas verificar se dá para abrir em 'r'.
+	char id_book_str[15];
+	printf("Insert ISBN Book: ");
+	read_n_chars(14, id_book_str);
+	if(strlen2(id_book_str)!=13){
+		printf("ISBN is not valid!\n");
+		pause_();
+		return 2;
+	}
+	BOOK* book=get_book_by_id(BOOK_ARCHIVE_DIR, id_book_str);
+	if(book==NULL){
+		printf("Book not found!\n");
+		pause_();
+		return 2;
+	}
+	book->requested_by = 0;
+	while (BOOK_ARCHIVE_DIR[(uint8_t) book->requested_by] != 0x00) book->requested_by++;
+	char file_path[(uint8_t) book->requested_by + 18]; // 18 = 13 + 5 + .csv + \0
+	snprintf(file_path, sizeof(file_path), "%s%13lu.csv", BOOK_ARCHIVE_DIR, book->uid);
+	delete_linked_list(book->queue_for_students, free);
+	free(book);
+	printf("%s\n", file_path);
+	 if (remove(file_path) == 0) {
+        printf("Book removed successfully!\n");
+    } else {
+        printf("Error: Unable to delete the file.\n");
+    }
+	
+	pause_();
+	return 0;
+}
+
 uint8_t check_book_info(){
 	char id_book_str[15];
 	printf("Insert ISBN Book: ");
@@ -731,6 +764,7 @@ void mng_biblman_account(){
 		if(_escolha_menu==0) break;
 		switch(_escolha_menu){
 			case 1: add_book(); break;
+			case 2: remove_book(); break;
 			case 5: regist(); break;
 			case 6: reset_password(); break;
 			case 7: change_password(); break;
