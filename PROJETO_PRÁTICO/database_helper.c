e8f4ab3c07f333c8e0a5a5c0337c049ccca80405
PdvSobral <->
2025-06-14 17:51:36 +0000

    Removed obsolete function. Sry, MacUser... But your legacy lives on in the loggin functions on 'main.c'.

--- PROJETO_PRÁTICO/database_helper.c
+++ PROJETO_PRÁTICO/database_helper.c
@@ -365,44 +365,6 @@ void print_book_data(BOOK* data){
 	if(data->queue_for_students->size!=0) printf("First on queue %u\n", *((uint32_t*)data->queue_for_students->head->data));
 }
 
-uint8_t log_requisition(ACCOUNT* user, BOOK* book, const char* file_path, const char* archive_folder){
-	FILE* file;
-    file = fopen(file_path, "a");
-    if (file == NULL) {
-        return 0;
-    }
-    DATE date;
-	date = get_current_date(date);
-	fprintf(file, "%02d/%02d/%04d,", date.day, date.month, date.year);
-	fprintf(file, "%d,", user->uid);
-
-    // TODO: Aqui está uma cópia da função print_book_name, altera à vontade.
-    char buffer[7];
-	buffer[6] = 0x00;
-	buffer[0] = 0x00;
-	while(archive_folder[(uint8_t) buffer[0]]!=0x0A) buffer[0]++;
-	char file_path2[buffer[0]+18];
-	snprintf(file_path2, sizeof(file_path2), "%s" ISBN_FORMAT ".csv", archive_folder, book->uid);
-	FILE* file2 = fopen(file_path, "rb");
-	if(file2==NULL){printf("ERROR!\n"); fflush(stdout); return 1;}
-	fseek(file, 6, SEEK_SET);
-	int8_t bytesRead;
-	while(1){
-		bytesRead = fread(buffer, 1, 6, file2);
-		if (bytesRead == 0) break;
-		for (bytesRead--; bytesRead >= 0; bytesRead--) {
-			if (buffer[bytesRead] == 0x0A) {
-				buffer[bytesRead] = 0x00;
-				fprintf(file, "%s\n", buffer); // TODO: MUDA ESTA LINHA
-				fclose(file);
-				return 0;
-			}
-		}
-		fprintf(file, "%s", buffer); // TODO: MUDA ESTA LINHA
-	}
-	return 1; // Error, no name found
-}
-
 void shift_bytes_up(FILE* file, uint32_t offset){
 	uint32_t current_pos = ftell(file);
 	uint8_t buffer_size = 12;
